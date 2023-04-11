ce47b710cf12bf1bb3266dd812685c6da3188add
Or <gindesor@gmail.com>
2023-04-11 18:08:49 +0300

    * Add TODOs to Evaluate.py and comments explaining preprocessing for divorce dataset

--- Assignment1/Evaluate.py
+++ Assignment1/Evaluate.py
@@ -19,7 +19,14 @@ if __name__ == "__main__":
     np.random.shuffle(divorce_data)
     X = divorce_data[:, :-1]
     y = divorce_data[:, 1]
+    # Each feature in X is a question response between 0 and 4
+    # We'll discretize the answers into two bins, 0 = [0,1] and 1 = [2,3,4]
     est = KBinsDiscretizer(n_bins=2, encode='ordinal', strategy='uniform').fit(X)
     X_transformed = est.transform(X)
     results[data_name] = evaluate(X_transformed, y)
     pass
+    # TODO: repeat this process for 4 more classification datasets
+    # TODO: implement weights & biases package usage (see evaluate function)
+    # TODO: write detailed report -
+    #  the report should include a table that compares the predictive performance of the various methods,
+    #  in the following structure: Dataset | Method | Evaluation metric | Evaluation Value | Fit Runtime (in ms)
